426b23e28f38d50a156d304d8c15ea7b1e88d017
su-de-sh <sudesh7443@gmail.com>
2022-06-30 11:32:01 +0545

    feat:made board

--- GameOfLife.js
+++ GameOfLife.js
@@ -10,17 +10,15 @@ class GameOfLife {
    */
 
   makeBoard() {
-    // TODO: Create and return an 2D Array 
-    // with `this.heigh` as rows and `this.width` as cols.
-    // For example, given a height of 4 and a width of 3, it will generate:
-    // [
-    //  [0, 0, 0],
-    //  [0, 0, 0],
-    //  [0, 0, 0],
-    //  [0, 0, 0],
-    // ]
+    const board = [];
+    for (let i = 0; i < this.height; i++) {
+      board.push([]);
+      for (let j = 0; j < this.width; j++) {
+        board[i].push(0);
+      }
+    }
+    return board;
   }
-
 
   /**
    * Return the amount of living neighbors around a given coordinate.
@@ -30,17 +28,16 @@ class GameOfLife {
     // TODO: Return the count of living neighbors.
   }
 
-
   /**
    * Given the present board, apply the rules to generate a new board
    */
-  
+
   tick() {
     const newBoard = this.makeBoard();
     // TODO: Here is where you want to loop through all the cells
     // on the existing board and determine, based on it's neighbors,
-    // whether the cell should be dead or alive in the new board 
-    // (the next iteration of the game) 
+    // whether the cell should be dead or alive in the new board
+    // (the next iteration of the game)
     //
     // You need to:
     // 1. Count alive neighbors for all cells
